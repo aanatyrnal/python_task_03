56513dc72d80d87ee050bdc9946c007646c4804e
aanatyrnal <cacina_ang1891@mail.ru>
2022-03-07 01:00:20 +0300

    Задания к уроку №3

---
+++ task_02.py
@@ -0,0 +1,22 @@
+# *(вместо задачи 1) Доработать предыдущую функцию в num_translate_adv(): реализовать
+# корректную работу с числительными, начинающимися с заглавной буквы — результат тоже
+# должен быть с заглавной. Например:
+# >>> num_translate_adv("One")
+# "Один"
+# >>> num_translate_adv("two")
+# "два"
+
+dict_tr = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
+               'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
+
+
+def num_translate(word):
+    if word[0] == word[0].upper():
+        word = word.lower()
+        return dict_tr[word].capitalize()
+    else:
+        return dict_tr[word]
+
+
+print(num_translate('nine'))
+print(num_translate('Nine'))
